e626fe89099ffb43780454776b44663483d95958
Shivashankar <shivably04sdst@gmail.com>
2020-03-07 14:07:30 +0530

    pages updated

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>ಆಧುನಿಕ ಕರ್ನಾಟಕದ ಬೌದ್ಧಿಕ ಇತಿಹಾಸ</title>
+    <meta charset="utf-8" />
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <link rel="shortcut icon" href="php/img/favicon.ico">
+  </head>
+  <body>
+<?php  header("Location: http://localhost/aakarabharati/php/index.php"); ?>
+</body>
+</html>
--- php/footer.php
+++ php/footer.php
@@ -1,5 +1,5 @@
 		   <footer>
-			  <span>Digital Archives in Indian Languages&nbsp;(DAIL)</span>
+			  <span>Sriranga Digital Software Technologies Private Limited</span>
           </footer>
 			<script src="js/intermediate/jquery-2.1.1.js"></script>
 	<script src="js/intermediate/jquery.mobile.custom.min.js"></script>
